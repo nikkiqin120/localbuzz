3d36641fee3a843efcd42c711778d6570eb319cc
Nikki Qin <nikkiqin120@gmail.com>
2025-04-06 16:04:02 -0700

    Add Google Analytics 4 tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "@/components/Layout";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-YBM27K0QRR`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YBM27K0QRR');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} min-h-full antialiased`}>
         <Layout>{children}</Layout>
       </body>
