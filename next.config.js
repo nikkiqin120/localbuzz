45101e21fa39754a2183a3ea6e595fb07ed38b33
Nikki Qin <nikkiqin120@gmail.com>
2025-04-21 17:28:52 -0700

    pic moved to imgur

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   images: {
-    domains: ['vancouverlocalbuzz.com', 'localhost'],
+    domains: ['vancouverlocalbuzz.com', 'localhost', 'i.imgur.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
